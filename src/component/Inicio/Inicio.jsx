0bc183d60dd29895ae1fdbf8171b84c3cca78a8a
lauty-shock <87452876+lauty-shock@users.noreply.github.com>
2022-09-22 16:06:26 -0300

    Inicio

--- src/component/Inicio/Inicio.jsx
+++ src/component/Inicio/Inicio.jsx
@@ -3,6 +3,11 @@ import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 import css from "./Inicio.module.css";
 
+import { BsGithub } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { BsFillEnvelopeFill } from "react-icons/bs";
+
 export default function Inicio() {
   const [text] = useTypewriter({
     words: ["Front-End Developer", "Back-End Developer", "FullStack Developer"],
@@ -10,14 +15,30 @@ export default function Inicio() {
   });
   return (
     <div className={css.container}>
-      <section className={css.containerName}>
-        <span className={css.name}>Lautaro Perez</span>
-        <span className={css.servicios}>
-          {text}
-          <Cursor />
-        </span>
+      <section className={css.border}>
+        <section className={css.containerName}>
+          <span className={css.name}>Lautaro Perez</span>
+          <span className={css.servicios}>
+            {text}
+            <Cursor />
+          </span>
+        </section>
+        <button className={css.btn}>Más sobre mi</button>
       </section>
-      <button className={css.btn}>Más sobre mi</button>
+      <section className={css.contac}>
+        <button className={css.btnContac}>
+          <BsGithub />
+        </button>
+        <button className={css.btnContac}>
+          <BsWhatsapp />
+        </button>
+        <button className={css.btnContac}>
+          <BsLinkedin />
+        </button>
+        <button className={css.btnContac}>
+          <BsFillEnvelopeFill />
+        </button>
+      </section>
     </div>
   );
 }
