ec9024edca534bc145526289bd7f4459cf3e4c90
lauty-shock <87452876+lauty-shock@users.noreply.github.com>
2022-09-22 18:28:53 -0300

    Forma de proyectos

--- src/component/Proyectos/Proyectos.jsx
+++ src/component/Proyectos/Proyectos.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 import css from "./Proyectos.module.css";
 
 export default function Proyectos() {
   return (
     <div id="Proyectos" className={css.Proyectos}>
-        <h1>Proyectos</h1>
+      <h1>Proyectos</h1>
+
+      <div className={css.containerProyectos}>
+        <div className={css.card}></div>
+        <div className={css.card}></div>
+        <div className={css.card}></div>
+      </div>
     </div>
-  )
+  );
 }
